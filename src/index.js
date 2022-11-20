07f47df3be13758dab4b5607cf9ac2166f5943bb
khaliasjohnson25 <115362382+khaliasjohnson25@users.noreply.github.com>
2022-11-20 08:52:04 -0500

    added forecast script

--- src/index.js
+++ src/index.js
@@ -28,7 +28,37 @@ function formatDay(timestamp) {
 
   return days[day];
 }
+function displayForecast(response) {
+  let forecast = response.data.daily;
 
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+    ' <div class="col-2">
+      <div class="forecast-date">${formatDay(forecastDay.time * 1000)}</div>  
+          src="${forecastDay.condition.icon_url}"
+          alt=""
+          width="42" /><div class="weather-forecast-temperatures">
+            <span class="weather-forecast-temperature-max">${Math.round(
+              forecastDay.temperature.maximum
+            )}°</span>
+            <span class="weather-forecast-temperature-min">${Math.round(
+              forecastDay.temperature.minimum
+            )}°</span>
+          </div>
+    </div>`;  
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  console.log(response.data);
+}
 
 
 function getForecast(coordinates) {
